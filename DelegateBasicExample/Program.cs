77a2249c3a1c204f7b1221e6ab247623eaaaf16f
priyankas0907 <priyankasinha0301@gmail.com>
2023-08-26 17:10:35 +0530

    basic delegate example

---
+++ DelegateBasicExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace DelegateBasicExample
+{
+    internal class Program
+    {
+        delegate void LogDel(string text);
+        static void Main(string[] args)
+        {
+            LogDel logDel = new LogDel(LogToFile);DigitShapes 
+            logDel("PS");
+        }
+
+        static void LogToScreen(string text)
+        {
+            Console.WriteLine($"{DateTime.Now}: {text}");
+        }
+
+        static void LogToFile(string text)
+        {
+            using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
+            {
+                sw.WriteLine($"{DateTime.Now} : {text}");
+            }
+        }
+    }
+}
